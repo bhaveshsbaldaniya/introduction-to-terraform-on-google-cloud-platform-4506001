b7471e137a1bd56b3be0720678033edddeeedcf2
bhaveshsbaldaniya <166301169+bhaveshsbaldaniya@users.noreply.github.com>
2024-07-06 18:05:22 +0530

    updated file

--- main.tf
+++ main.tf
@@ -1,36 +1,35 @@
 module "app_network" {
   source  = "terraform-google-modules/network/google"
-  version = "9.1.0"
- 
+  version = "8.1.0"
+
   network_name = "${var.network_name}-network"
-  project_id = var.project_id
+  project_id   = var.project_id
 
   subnets = [
-        {
-          subnet_name      = "${var.network_name}-subnet0"
-          subnet_ip        = var.network_ip_range
-          subnet_region    = var.region
-        }
+    {
+      subnet_name   = "${var.network_name}-subnet0"
+      subnet_ip     = var.network_ip_range
+      subnet_region = var.region
+    }
   ]
 
   ingress_rules = [
     {
-        name                    = "${var.network_name}-web"
-        description             = "Inbound web"
-
-        source_ranges           = ["0.0.0.0/0"]
-        target_tags             = ["${var.network_name}-web"]
+      name          = "${var.network_name}-web"
+      description   = "Inbound web"
     
-    allow = [
-      {
-        protocol = "tcp"
-        ports    = ["80","443"]
-      }
-    ]
-  }
- ]
-}
+      source_ranges = ["0.0.0.0/0"]
+      target_tags   = ["${var.network_name}-web"]
 
+      allow = [
+        {
+          protocol = "tcp"
+          ports    = ["80","443"]
+        }
+      ]
+    }
+  ]
+}
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
@@ -38,26 +37,24 @@ data "google_compute_image" "ubuntu" {
   family      = var.image_family
 }
 
-resource "google_compute_instance" "app" {
+resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
 
- tags = ["$(var.network_name)-web"]
-  
+  tags = ["${var.network_name}-web"]
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
   }
-   network_interface {
-   subnetwork = module.app_network.subnets.name[0]
+  network_interface {
+   subnetwork = module.app_network.subnets_names[0]
    access_config {
       # Leave empty for dynamic public IP
     }
   }  
- 
- metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
 
- allow_stopping_for_update = true
-
+  metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
+  allow_stopping_for_update = true
 }
