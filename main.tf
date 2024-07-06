87f3fd10fb8a36b48777049d83efee313a46b7b0
bhaveshsbaldaniya <166301169+bhaveshsbaldaniya@users.noreply.github.com>
2024-07-06 17:45:27 +0530

    updated metadata startup script

--- main.tf
+++ main.tf
@@ -55,7 +55,9 @@ resource "google_compute_instance" "app" {
       # Leave empty for dynamic public IP
     }
   }  
- metadata_startup_project = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
+ 
+ metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
+
  allow_stopping_for_update = true
 
 }
