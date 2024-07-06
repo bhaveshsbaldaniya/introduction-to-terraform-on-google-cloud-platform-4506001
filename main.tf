212be6e5f696862f6d8a221e4114063ee83b4f4a
bhaveshsbaldaniya <166301169+bhaveshsbaldaniya@users.noreply.github.com>
2024-07-06 18:53:48 +0530

    updated main

--- main.tf
+++ main.tf
@@ -1,6 +1,29 @@
-module "qa"{
-    source =  "./modules/blog"
+module "qa" {
+  source = "./modules/blog"
 
-    app_name = "qa-blog"
-    network_name = "qa"
+  project_id = var.project_id
+  region     = var.region
+
+  app_name     = "qa-blog"
+  network_name = "qa"
+}
+
+module "staging" {
+  source = "./modules/blog"
+
+  project_id = var.project_id
+  region     = var.region
+
+  app_name     = "staging-blog"
+  network_name = "staging"
+}
+
+module "prod" {
+  source = "./modules/blog"
+
+  project_id = var.project_id
+  region     = var.region
+
+  app_name     = "prod-blog"
+  network_name = "prod"
 }
