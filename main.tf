85ff706742edbae5f795a98ce00a9fbcfb2ef768
bhaveshsbaldaniya <166301169+bhaveshsbaldaniya@users.noreply.github.com>
2024-07-06 17:04:34 +0530

    Variables

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 resource "google_compute_network" "app" {
-  name                    = "app"
+  name                    = var.network_name
   auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "app" {
-  name          = "app"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-west1"
+  name          = var.network_name
+  ip_cidr_range = var.network_ip_range
+  region        = var.region
   network       = google_compute_network.app.id
  }
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
-  project     = "ubuntu-os-cloud" 
-  family      = "ubuntu-2204-lts"
+  project     = var.image_project 
+  family      = var.image_family
 }
 
 resource "google_compute_instance" "app" {
-  name         = "app"
-  machine_type = "e2-micro"
+  name         = var.app_name
+  machine_type = var.machine_type
 
   
   boot_disk {
@@ -26,12 +26,12 @@ resource "google_compute_instance" "app" {
     }
   }
   network_interface {
-   subnetwork = "app"
+   subnetwork = var.network_name
    access_config {
       # Leave empty for dynamic public IP
     }
   }  
 
  allow_stopping_for_update = true
- #checking pending
+
 }
